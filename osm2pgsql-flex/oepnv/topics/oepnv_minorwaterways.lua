2ea6fd4ee83aa16ad759351014680d118ae2902d
Amanda McCann <amanda@technomancy.org>
2025-11-26 16:54:52 +0100

    refactor
    make code smaller.
    make data correct

--- osm2pgsql-flex/oepnv/topics/oepnv_minorwaterways.lua
+++ osm2pgsql-flex/oepnv/topics/oepnv_minorwaterways.lua
@@ -15,46 +15,26 @@ themepark:add_table({
     columns = themepark:columns({
         { column = "name", type = "text" },
         { column = "width", type = "smallint" },
-        { column = "bridge", type = "bool" },
-        { column = "tunnel", type = "bool" },
+        { column = "bridge", type = "bool", not_null = true },
+        { column = "tunnel", type = "bool", not_null = true },
         { column = "layer", type = "smallint" },
-        { column = "type", type = "text" },
+        { column = "type", type = "text", not_null = true },
     }),
     tiles = {
         minzoom = 8,
     },
 })
 
--- Get the waterwaytype
-local function get_waterway(object)
-    local waterwaytag = object.tags.waterway
-    local waterways = nil
-
-    if waterwaytag == "stream" then
-        waterways = "stream"
-    elseif waterwaytag == "drain" then
-        waterways = "drain"
-    elseif waterwaytag == "ditch" then
-        waterways = "ditch"
-    end
-
-    return waterways
-end
-
------------------------------------------------------------------------------
-
 themepark:add_proc("way", function(object)
-    local waterways = get_waterway(object)
-    if waterways then
+    if is_in(object.tags["waterway"], { "stream", "drain", "ditch" }) then
         themepark:insert("oepnv_minorwaterways", {
-
-            geom = object:as_linestring(),
             name = object.tags["name"],
-            bridge = object.tags.bridge,
-            tunnel = object.tags.tunnel,
-            width = object.tags.width,
-            layer = object.tags.layer,
-            type = waterways,
+            bridge = object.tags["bridge"] or "no",
+            tunnel = object.tags["tunnel"] or "no",
+            width = object.tags["width"],
+            layer = object.tags["layer"],
+            type = object.tags["waterway"],
+            geom = object:as_linestring(),
         })
     end
 end)
--- osm2pgsql-flex/oepnv/topics/oepnv_waterways.lua
+++ osm2pgsql-flex/oepnv/topics/oepnv_waterways.lua
@@ -15,42 +15,28 @@ themepark:add_table({
     columns = themepark:columns({
         { column = "name", type = "text" },
         { column = "width", type = "smallint" },
-        { column = "bridge", type = "bool" },
-        { column = "tunnel", type = "bool" },
+        { column = "bridge", type = "bool", not_null = true },
+        { column = "tunnel", type = "bool", not_null = true },
         { column = "layer", type = "smallint" },
-        { column = "type", type = "text" },
+        { column = "type", type = "text", not_null = true },
     }),
     tiles = {
         minzoom = 8,
     },
 })
 
-function get_waterway(object)
-    local waterwaytag = object.tags.waterway
-    local waterways = nil
-
-    if waterwaytag == "river" then
-        waterways = "river"
-    elseif waterwaytag == "canal" then
-        waterways = "canal"
-    end
-    return waterways
-end
-
 -----------------------------------------------------------------------------
 -- Removed relations and nodes because it didn't seem necesarry
 themepark:add_proc("way", function(object)
-    local waterways = get_waterway(object)
-    if waterways then
+    if is_in(object.tags["waterway"], { "river", "canal" }) then
         themepark:insert("oepnv_waterways", {
-
-            geom = object:as_linestring(),
             name = object.tags["name"],
-            bridge = object.tags.bridge,
-            tunnel = object.tags.tunnel,
-            width = object.tags.width,
-            layer = object.tags.layer,
-            type = waterways,
+            bridge = object.tags["bridge"] or "no",
+            tunnel = object.tags["tunnel"] or "no",
+            width = object.tags["width"],
+            layer = object.tags["layer"],
+            type = object.tags["waterway"],
+            geom = object:as_linestring(),
         })
     end
 end)
